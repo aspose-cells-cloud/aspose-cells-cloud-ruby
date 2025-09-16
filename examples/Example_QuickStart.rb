fc215038037485fb43d86caadac791b698f09e8c
roy.wang <roy.wang@aspose.com>
2025-09-16 10:29:45 +0800

    ISSUE-CELLSCLOUD-13424: Development of cells cloud sdk 25.9.

--- examples/Example_QuickStart.rb
+++ examples/Example_QuickStart.rb
@@ -3,6 +3,6 @@ require 'bundler'
 require 'aspose_cells_cloud'
 
 @instance = AsposeCellsCloud::CellsApi.new(ENV['CellsCloudClientId'], ENV['CellsCloudClientSecret']);
-request = AsposeCellsCloud::ConvertSpreadsheetRequest.new(:Spreadsheet=>'EmployeeSalesSummary.xlsx',:format=>format);
+request = AsposeCellsCloud::ConvertSpreadsheetRequest.new(:Spreadsheet=>'EmployeeSalesSummary.xlsx',:format=>'pdf');
 response = @instance.convert_spreadsheet(request);
-FileUtils.cp(response.path, 'EmployeeSalesSummary.csv')
+FileUtils.cp(response.path, 'EmployeeSalesSummary.pdf')
